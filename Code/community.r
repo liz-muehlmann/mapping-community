82f7e5ea09b33b2b92f6ca1269f28ec2b17543e7
Liz <liz.muehlmann@uci.edu>
2023-03-14 15:28:57 -0700

    init r file, add data

---
+++ Code/community.r
@@ -0,0 +1,15 @@
+############################# Community ties by state #############################
+## Building my methodological skills, one map at a a time.
+##
+## Data:
+## United States Citizenship, Involvement, Democracy (CID) Survey, 2006
+##      - The data set investigates "American civic engagement, social capital, 
+##          and democracy in comparative perspective, and it provides perspective 
+##          on citizen participation in both the public and private realms"
+##      - https://www.icpsr.umich.edu/web/civicleads/studies/04607#
+##
+## State boundary data from the US Census, downloaded using the Tigris package
+##  https://cran.r-project.org/web/packages/tigris/
+## 
+## Voter turn out from 2004 - 2020
+##  Slider using shiny, liz.
